312b48ec92b6651d0ce12b432766969e9efe66bf
alharithalk <kindaking99@gmail.com>
2026-06-23 09:58:44 +0400

    added layers

--- src/main/java/com/Invetory/mangment/Inventory/Order/Management/repository/OrderItemRepository.java
+++ src/main/java/com/Invetory/mangment/Inventory/Order/Management/repository/OrderItemRepository.java
@@ -1,4 +1,7 @@
 package com.Invetory.mangment.Inventory.Order.Management.repository;
 
-public interface OrderItemRepository {
+import com.Invetory.mangment.Inventory.Order.Management.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
--- src/main/java/com/Invetory/mangment/Inventory/Order/Management/repository/OrderRepository.java
+++ src/main/java/com/Invetory/mangment/Inventory/Order/Management/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.Invetory.mangment.Inventory.Order.Management.repository;
 
-public interface OrderRepository {
+import com.Invetory.mangment.Inventory.Order.Management.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
